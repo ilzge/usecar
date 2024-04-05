da0d7452070ea88ba9db21d4081a94a88fdc7138
ilzge <fluxway@163.com>
2024-04-05 12:14:34 +0800

    Update usecaryyy.js

--- usecaryyy.js
+++ usecaryyy.js
@@ -1,1 +1,4 @@
+[rewrite_local]
 https://app2.patentexam.com.cn/nbtaxi/api/punch/queryWorkPointList url response-body "longitude":"116.307095","latitude":"39.827151","rangee":" response-body "longitude":"116.307095","latitude":"39.827151","rangee":"300000000000
+[mitm] 
+hostname = *.patentexam.com.cn
